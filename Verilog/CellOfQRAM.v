dcb676aac48c843ea5c3d984fb84d3b812195bc0
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-05-22 09:33:08 +0900

    Create CellOfQRAM.v

---
+++ Verilog/CellOfQRAM.v
@@ -0,0 +1,50 @@
+`include "DigitSupply.vh"
+
+module CellOfQRAM(
+    output outputData,
+    input ReadEdge,
+    input inputData,
+    input WriteEdge
+  );
+    tri outputData;
+    tri ReadEdge;
+    tri inputData;
+    tri WriteEdge;
+
+    tri West;
+    
+    tri North;
+    tri South;
+
+    tri East;
+
+    tri NorthToSouth1;
+    tri SouthToNorth1;
+
+    trireg NorthToSouth2;
+    trireg SouthToNorth2;
+    
+    _nmos Read(West, inputData, ReadEdge);
+    
+    assign North=West;
+
+    assign South=West;
+
+    _nmos ChargeNorth(NorthToSouth1, North, Vdd);
+
+    _nmos ChargeSouth(SouthToNorth1, South, Vdd);
+
+    assign NorthToSouth2=NorthToSouth1;
+
+    assign SouthToNorth2=SouthToNorth1;
+
+    assign South=NorthToSouth2;
+    
+    assign North=SouthToNorth2;
+
+    assign East=North;
+
+    assign East=South;
+
+    _nmos Read(outputData, East, WriteEdge);
+endmodule
