abc26f1032a440112253c726b35bc0276f373329
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-06-05 09:49:15 +0900

    Update DifferentialQbit.v

--- Verilog/DifferentialQbit.v
+++ Verilog/DifferentialQbit.v
@@ -1,1 +1,28 @@
+`include "DigitSupply.vh"
 
+module DifferentialQBit(
+    inout QBitN,
+    inout QBitS
+);
+    tri QBitN;
+    tri QBitS;
+  
+    tri NorthToSouth1;
+    tri SouthToNorth1;
+
+    trireg NorthToSouth2;
+    trireg SouthToNorth2;
+    
+    _nmos ChargeNorth(NorthToSouth1, QBitN, Vdd);
+
+    _nmos ChargeSouth(SouthToNorth1, QBitS, Vdd);
+
+    assign NorthToSouth2=NorthToSouth1;
+
+    assign SouthToNorth2=SouthToNorth1;
+
+    assign QBitS=NorthToSouth2;
+    
+    assign QBitN=SouthToNorth2;
+    
+endmodule
