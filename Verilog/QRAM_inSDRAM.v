f33562731348c49f2816c905401a04cf7d319aa8
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-06-06 07:58:08 +0900

    Create QRAM_inSDRAM.v

---
+++ Verilog/QRAM_inSDRAM.v
@@ -0,0 +1,45 @@
+`include "DigitSupply.vh"
+
+module QRAM_inSDRAM (
+    output outputQBit,
+    input Read,
+    input inputQBit,
+    input Write,
+    input DDRClockP,
+    input DDRClockN
+);
+
+endmodule
+
+module CellOfQRAM(
+    output outputData,
+    input ReadEdge,
+    input inputData,
+    input WriteEdge
+);
+    tri outputData;
+    tri ReadEdge;
+    tri inputData;
+    tri WriteEdge;
+
+    tri West;
+    
+    tri North;
+    tri South;
+
+    tri East;
+
+    _nmos Write(West, inputData, WriteEdge);
+    
+    assign North=West;
+
+    assign South=West;
+
+    DifferentialQBit UseQBit(North,South);
+    
+    assign East=North;
+
+    assign East=South;
+
+    _nmos Read(outputData, East, ReadEdge);
+endmodule
