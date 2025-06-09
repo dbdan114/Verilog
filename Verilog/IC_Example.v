22a7050ffd8771c6681213a4576e66b041d80f84
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-06-10 08:00:48 +0900

    Update IC_Example.v

--- Verilog/IC_Example.v
+++ Verilog/IC_Example.v
@@ -2,13 +2,25 @@
 `include "DigitSupply.vh"
 
 module IC_Example (
+    /* To Write Your Signal*/
 
-
-
+    /* To Write Your Signal*/
 );
-  tri ReadQBit;
-  tri WriteQBit;
+  tri QBitToRead;
+  tri QBitToWrite;
+  tri WrtingToQBit;
+  tri AddressQBit;
 
+  tri QRAM_DDRClockP;
+  tri QRAM_DDRClockN;
+  
+  assign QBitToWrite = rVss;
+  assign WrtingToQBit = rVdd;
+  
+  QRAM_inSDRAM MakeQRAM(QBitToRead, Vdd, QBitToWrite, WrtingToQBit, QRAM_DDRClockP, QRAM_DDRClockN);
+
+  
+  
 endmodule
 module QRAM_inSDRAM (
     output outputQBit,
