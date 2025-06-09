47e2f2beecd2f1b7a1bb7dfacaf6f1032bc7ccdd
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-06-10 07:44:55 +0900

    Update IC_Example.v

--- Verilog/IC_Example.v
+++ Verilog/IC_Example.v
@@ -1,1 +1,23 @@
 
+`include "DigitSupply.vh"
+
+module IC_Example (
+
+
+
+);
+  tri ReadQBit;
+  tri WriteQBit;
+
+endmodule
+module QRAM_inSDRAM (
+    output outputQBit,
+    input Read,
+    input inputQBit,
+    input Write,
+    input AddressQBit,
+    input DDRClockP,
+    input DDRClockN
+);
+endmodule
+  
