6d7c905c966c25e3834f05a58b08924a0ac46998
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-07-06 09:07:04 +0900

    Update _nor.v

--- Verilog/_nor.v
+++ Verilog/_nor.v
@@ -2,9 +2,11 @@ module _nor#(
 parameter INPUT_WIDTH = 1
 ) (
     output outputData,
+    input [1:0] DigitSupply,
     input [INPUT_WIDTH-1:0] inputData
     );
     tri outputData;
+    tri [1:0] DigitSupply;
     tri [INPUT_WIDTH-1:0] inputData;
     
     tri  [INPUT_WIDTH:0] Temp;
@@ -13,7 +15,7 @@ parameter INPUT_WIDTH = 1
     generate
         for(Index=0; Index < INPUT_WIDTH; Index = Index + 1)
         begin:Place_and_nB_to_A
-            and_nB_to_A Paste(Temp[Index+1],2'b10,Temp[Index],inputData[Index]);
+            and_nB_to_A Paste(Temp[Index+1],DigitSupply,Temp[Index],inputData[Index]);
         end
     endgenerate
     assign outputData=Temp[INPUT_WIDTH];
