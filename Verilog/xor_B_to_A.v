b43f17fb89c82ffdaadd5e6b7f1caf3cb9833c84
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-07-06 09:15:34 +0900

    Update xor_B_to_A.v

--- Verilog/xor_B_to_A.v
+++ Verilog/xor_B_to_A.v
@@ -1,14 +1,20 @@
 module xor_B_to_A(
     output outputData,
+    input [1:0] DigitSupply,
     input A,
     input B
     );
+    tri outputData;
+    tri [1:0] DigitSupply;
+    tri A;
+    tri B;
+    
     tri TempNor;
     tri TempAnd;
     
-    _nor #(.INPUT_WIDTH(2)) GetNor(TempNor,{B,A});
-    _and #(.INPUT_WIDTH(2)) GetAnd(TempAnd,{B,A});
+    _nor #(.INPUT_WIDTH(2)) GetNor(TempNor,DigitSupply,{B,A});
+    _and #(.INPUT_WIDTH(2)) GetAnd(TempAnd,DigitSupply,{B,A});
     
-    _nor #(.INPUT_WIDTH(2)) GetXor(outputData, {TempNor,TempAnd});
+    _nor #(.INPUT_WIDTH(2)) GetXor(outputData,DigitSupply,{TempNor,TempAnd});
         
 endmodule
