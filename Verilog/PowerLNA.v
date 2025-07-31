4db616378f19b09d73d5bd3ed57315888762e31d
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-08-01 07:27:47 +0900

    Update PowerLNA.v

--- Verilog/PowerLNA.v
+++ Verilog/PowerLNA.v
@@ -14,8 +14,14 @@ module PowerLNA
   tri Transmit;
   tri EnableTransmit;
 
+  tri TempJunction;
+  
   DifferentialQBit MakeSupplySignal0(SupplyDigit[1], SupplyDigit[1]);
   DifferentialQBit MakeSupplySignal1(SupplyDigit[1], SupplyDigit[0]);
   
+  DifferentialQBit MakePullSignal1(SupplyDigit[0], PullDigit[1]);
+  DifferentialQBit MakePullSignal0(SupplyDigit[0], PullDigit[0]);
+
+  DifferentialQBit GetInOutSignal(TempJunction, SupplyDigit[1]);
   
 endmodule  
