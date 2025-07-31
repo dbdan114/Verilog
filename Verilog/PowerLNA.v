8ee8553ae52e34311cb7a19c70e6134577e0003d
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-08-01 07:16:53 +0900

    Update PowerLNA.v

--- Verilog/PowerLNA.v
+++ Verilog/PowerLNA.v
@@ -1,5 +1,11 @@
 `include "DigitSupply.vh"
 
 module PowerLNA
-  (
-    
+(
+  output Receive,
+  input EnaleReceive,   // Approximately 2.1 V
+  input Transmit,
+  input EnableTransmit, // Approximately 2.1 V
+);
+
+endmodule  
