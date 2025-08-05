84e489a5d4a3dddd249def4177223354c3a4d5d1
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-08-05 09:11:56 +0900

    Update and rename OpAmpDO to OpAmpDO.v

---
+++ Verilog/OpAmpDO.v
@@ -0,0 +1,21 @@
+`include "DigitSupply.vh"
+
+module OpAmpDO
+(
+  input V_Plus,
+  input input_Plus,
+  output output_Plus,
+  output output_Minus,
+  input input_Minus,
+  input V_Minus
+);
+  tri V_Plus;
+  tri input_Plus;
+  tri output_Plus;
+  tri output_Minus;
+  tri input_Minus;
+  tri V_Minus;
+
+
+  
+endmodule
