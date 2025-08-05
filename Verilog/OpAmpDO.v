ba1566ecec00b6ebbaa7c9cae939c0779c199254
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-08-05 09:21:10 +0900

    Update OpAmpDO.v

--- Verilog/OpAmpDO.v
+++ Verilog/OpAmpDO.v
@@ -16,6 +16,10 @@ module OpAmpDO
   tri input_Minus;
   tri V_Minus;
 
-
+  DifferentialQBit FetchV_Plus(V_Plus,input_Plus);
+  DifferentialQBit FetchV_Minus(input_Minus,V_Minus);
+  DifferentialQBit Fetchinput_Plus(input_Plus,output_Plus);
+  DifferentialQBit Fetchinput_Minus(output_Minus,input_Minus);
+  DifferentialQBit output_Plus_To_output_Minus(output_Plus,output_Minus);
   
 endmodule
