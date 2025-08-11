06a35993f28d4f3586915af799648274560678fa
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-08-11 09:19:21 +0900

    Update SplitRadio.v

--- Verilog/SplitRadio.v
+++ Verilog/SplitRadio.v
@@ -18,13 +18,13 @@ module SplitRadio
   tri Temp_Minus3;
   tri Temp_Minus2;
   tri Temp_Minus1;
-  tri Trash;
+  tri NoConnect;
   tri V_Minus;
 
   DifferentialQBit FetchV_Plus(V_Plus,Receive);
-  DifferentialQBit FetchV_Minus(Trash,V_Minus);
+  DifferentialQBit FetchV_Minus(NoConnect,V_Minus);
   DifferentialQBit Fetchinput_Plus(Receive,Temp_Plus1);
-  DifferentialQBit FetchTrash(Temp_Minus1,Trash);
+  DifferentialQBit FetchTrash(Temp_Minus1,NoConnect);
   DifferentialQBit FetchTemp_Plus1(Temp_Plus1,Temp_Plus2);
   DifferentialQBit FetchTemp_Minus1(Temp_Minus2,Temp_Minus1);
   DifferentialQBit FetchTemp_Plus2(Temp_Plus2,Temp_Plus3);
