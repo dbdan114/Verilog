b7940a7de47c20f762f0201b9ba0762e3947671b
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-09-29 09:47:05 +0900

    Update MakeVoltPN.v

--- Verilog/MakeVoltPN.v
+++ Verilog/MakeVoltPN.v
@@ -7,5 +7,7 @@ module MakeVoltPN
 );
 
   tri [1:0] TempSupplyDigit = SupplyDigit;
-  
+  DifferentialQBit LinkVddVss(TempSupplyDigit[1],TempSupplyDigit[0]);
+  DifferentialQBit MakeV_Minus(TempSupplyDigit[0],V_Minus);
+  DifferentialQBit MakeV_Plus(V_Plus,TempSupplyDigit[1]);
 endmodule
