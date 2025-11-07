df62e79685695bc82df8cc04ec39249b4095a718
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-11-07 15:58:56 +0900

    Add SDR_QSRAM module with parameters and ports

--- Verilog/SDR_QSRAM.v
+++ Verilog/SDR_QSRAM.v
@@ -1,1 +1,27 @@
 `include "DigitSupply.vh"
+
+module SDR_QSRAM #(
+
+parameter ADDR_WIDTH = 1
+
+parameter DATA_WIDTH = 1
+
+)(
+
+    inout [DATA_WIDTH - 1 : 0] inoutData,
+
+    input [ADDR_WIDTH - 1 : 0] Address,
+
+    input Clock,
+
+    input Enable,
+
+    input Read,
+
+    input Write,
+
+    input Refresh
+
+);
+
+endmodule
