939f7fdf5e4a4a8e4e08ae39c8be070c28754255
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-11-08 16:36:39 +0900

    Define tri-state signals in IC_SDR_QSRAM module
    Added tri-state signals for Address, Clock, Enable, Read, Write, Refresh, and Crystal inputs.

--- Verilog/IC_SDR_QSRAM.v
+++ Verilog/IC_SDR_QSRAM.v
@@ -27,4 +27,23 @@ parameter DATA_WIDTH = 9
     input Crystal50Mhz2
 
 );
+    reg [DATA_WIDTH - 1 : 0] inoutData;
+
+    tri [ADDR_WIDTH - 1 : 0] Address;
+
+    tri Clock;
+
+    tri Enable;
+
+    tri Read;
+
+    tri Write;
+
+    tri Refresh;
+
+    tri Crystal50Mhz1;
+
+    tri Crystal50Mhz2;
+
+    
 endmodule 
