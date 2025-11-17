a6c45ce40e503e5d0cb2565605a60c7ef31cf783
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-11-17 14:13:17 +0900

    Update PowerLNA.v

--- Verilog/PowerLNA.v
+++ Verilog/PowerLNA.v
@@ -6,7 +6,7 @@ module PowerLNA
   output Receive,
   input EnaleReceive,   // Approximately 2.1 V
   input Transmit,
-  input EnableTransmit, // Approximately 2.1 V
+  input EnableTransmit // Approximately 2.1 V
 );
 
   tri Receive;
