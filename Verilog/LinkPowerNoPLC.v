fc99101f3ce1ed7272aeb97bd9e8573e59b71f42
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-11-29 14:55:58 +0900

    Update LinkPowerNoPLC.v
    dbdan@hanmail.net

--- Verilog/LinkPowerNoPLC.v
+++ Verilog/LinkPowerNoPLC.v
@@ -84,5 +84,37 @@ module LinkPowerNoPLC
     VplusFor5478,
     VminusFor5478
   );
+  OpAmpSEO Make12To36
+  (
+    VplusFor1236,
+    Clock100MhzP,
+    TIA_568B12To36,
+    TIA_568B12,
+    VminusFor1236
+  );
+  OpAmpSEO Redirect12To36
+  (
+    VplusFor1236,
+    TIA_568B12To36,
+    TIA_568B36,
+    Clock100MhzP,
+    VminusFor1236
+  );
+  OpAmpSEO Make54To78
+  (
+    VplusFor5478,
+    TIA_568B54,
+    TIA_568B54To78,
+    Clock100MhzP,
+    VminusFor5478
+  );
+  OpAmpSEO Rediect54To78
+  (
+    VplusFor5478,
+    Clock100MhzP,
+    TIA_568B78,
+    TIA_568B54To78,
+    VminusFor5478
+  );
   
 endmodule
