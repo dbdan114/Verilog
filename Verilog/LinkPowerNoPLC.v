2ce04d581f4f661d667b1fc8c040a3d86080e043
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-11-29 15:20:18 +0900

    Update LinkPowerNoPLC.v

--- Verilog/LinkPowerNoPLC.v
+++ Verilog/LinkPowerNoPLC.v
@@ -1,4 +1,66 @@
+`include "DigitSupply.vh"
 
+module LinkPowerNoPLC
+(
+  input Crystal50Mhz1,
+  input Crystal50Mhz2
+);
+  tri Crystal50Mhz1;
+  tri Crystal50Mhz2;
+  
+  tri Clock100MhzP;
+  tri Clock100MhzN;
+  
+  tri VplusForSplitRadio;
+  tri VminusForSplitRadio;
+
+  tri ToSplitRadio;
+
+  tri TIA_568B1236;
+  tri TIA_568B5478;
+
+  tri TIA_568B12;
+  tri TIA_568B36;
+  tri TIA_568B12To36;
+  
+  tri TIA_568B54;
+  tri TIA_568B78;
+  tri TIA_568B54To78;
+
+  tri VplusFor1236;
+  tri VminusFor1236;
+
+  tri VplusFor5478;
+  tri VminusFor5478;
+  
+  OscilateFromXtalDDR MakeClock100Mhz
+  (
+    Crystal50Mhz1,
+    Crystal50Mhz2,
+    Clock100MhzP,
+    Clock100MhzN
+  );
+  MakeVoltPN GetV_pnForSplitRadio
+  (
+    VplusForSplitRadio,
+    VminusForSplitRadio
+  );
+  PowerLNA GetRawPower
+  (
+    ToSplitRadio,
+    1'b1,
+    ToSplitRadio,
+    1'b1
+  );
+  SplitRadio MakeTwoSignal
+  (
+    VplusForSplitRadio,
+    ToSplitRadio,
+    TIA_568B1236,
+    TIA_568B5478,
+    VminusForSplitRadio
+  );
+  PostLNA Check12and36
   (
     TIA_568B1236,
     TIA_568B12,
@@ -54,9 +116,5 @@
     TIA_568B54To78,
     VminusFor5478
   );
-  DifferentialQbit Correct54To78
-  (
-    Vss,
-    TIA_568B54To78
-  );
+  
 endmodule
