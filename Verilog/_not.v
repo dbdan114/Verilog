71673fb6711a270980636aab6864edeca52ca932
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-12-08 18:25:42 +0900

    Refactor _not module for improved syntax

--- Verilog/_not.v
+++ Verilog/_not.v
@@ -1,15 +1,14 @@
-module _not(
-
-    output outputData,
-    input [1:0] DigitSupply,
-    input inputData
+module _not
+(
+  output outputData,
+  input [1:0] DigitSupply,
+  input inputData
+);
+  wire outputData;
+  tri [1:0] DigitSupply;
+  tri inputData;
     
-    );
-    wire outputData;
-    tri [1:0] DigitSupply;
-    tri inputData;
-    
-    _pmos For0(outputData,DigitSupply[1],inputData);
-    _nmos For1(outputData,DigitSupply[0],inputData);
+  _pmos For0(outputData,DigitSupply[1],inputData);
+  _nmos For1(outputData,DigitSupply[0],inputData);
     
 endmodule
