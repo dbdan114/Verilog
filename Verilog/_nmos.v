bd701607fb4efe7799abf6e9af04f90671569973
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-12-08 17:00:14 +0900

    Refactor _nmos module formatting and structure

--- Verilog/_nmos.v
+++ Verilog/_nmos.v
@@ -1,19 +1,21 @@
-module _nmos(
-    output outputData,
-    input inputData,
-    input Control
-    );
-    reg outputData;
-    tri inputData;
-    tri Control;
-    always @(*)
-    begin
+module _nmos
+(
+  output outputData,
+  input inputData,
+  input Control
+);
+  reg outputData;
+  tri inputData;
+  tri Control;
+  
+  always @(*)
+  begin
     if(Control == 1'b1)
-    outputData=inputData;
+      outputData=inputData;
     else if(Control ==1'b0)
-    outputData=1'bz;
+      outputData=1'bz;
     else
-    outputData= 1'bx;
-    end
-    endmodule
+      outputData= 1'bx;
+  end
+endmodule
     
