09149804da37d3bda11e144922a9cd9fe2358ea0
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-12-08 17:02:24 +0900

    Refactor _pmos module formatting and structure

--- Verilog/_pmos.v
+++ Verilog/_pmos.v
@@ -1,18 +1,20 @@
-module _pmos(
-    output outputData,
-    input inputData,
-    input Control
-    );
-    reg outputData;
-    tri inputData;
-    tri Control;
-    always @(*)
-    begin
+module _pmos
+(
+  output outputData,
+  input inputData,
+  input Control
+);
+  reg outputData;
+  tri inputData;
+  tri Control;
+  
+  always @(*)
+  begin
     if(Control == 1'b1)
-    outputData=1'bz;
+      outputData=1'bz;
     else if(Control == 1'b0)
-    outputData=inputData;
+      outputData=inputData;
     else
-    outputData= 1'bx;
-    end
-    endmodule
+      outputData= 1'bx;
+  end
+endmodule
