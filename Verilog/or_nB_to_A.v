8536ae29bf4cf9bddf0891fe1e92b5b9c57d47b5
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-12-16 08:18:18 +0900

    Replace nB with B in or_nB_to_A.v

--- Verilog/or_nB_to_A.v
+++ Verilog/or_nB_to_A.v
@@ -3,15 +3,15 @@ module or_nB_to_A(
     output outputData,
     input [1:0] DigitSupply,
     input A,
-    input nB
+    input B
     );
     wire outputData;
     tri [1:0] DigitSupply;
     tri A;
     
-    tri nB;
+    tri B;
     
-    _pmos For0(outputData,DigitSupply[1],nB);
-    _nmos For1(outputData,A,nB);
+    _pmos For0(outputData,DigitSupply[1],B);
+    _nmos For1(outputData,A,B);
     
 endmodule
