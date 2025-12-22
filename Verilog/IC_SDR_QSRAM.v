8095bf96080f18e5d96222edcc2165c35c3f9e94
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2025-12-22 11:12:29 +0900

    Refactor IC_SDR_QSRAM module parameter declaration

--- Verilog/IC_SDR_QSRAM.v
+++ Verilog/IC_SDR_QSRAM.v
@@ -1,52 +1,46 @@
 `include "DigitSupply.vh"
 
-module IC_SDR_QSRAM #(
-
-parameter ADDR_WIDTH = 33
-
-parameter DATA_WIDTH = 9
-
-)(
-
-    inout [DATA_WIDTH - 1 : 0] inoutData,
-
-    input [ADDR_WIDTH - 1 : 0] Address,
-
-    input Clock,
-
-    input Enable,
-
-    input Read,
-
-    input Write,
-
-    input Refresh,
-
-    input Crystal50Mhz1,
-
-    input Crystal50Mhz2
-
+module IC_SDR_QSRAM 
+#(
+  parameter ADDR_WIDTH = 33,
+  parameter DATA_WIDTH = 9
+)
+(
+  inout [DATA_WIDTH - 1 : 0] inoutData,
+  input [ADDR_WIDTH - 1 : 0] Address,
+  input Clock,
+  input Enable,
+  input Read,
+  input Write,
+  input Refresh,
+  input Crystal50Mhz1,
+  input Crystal50Mhz2
 );
     
-    reg [DATA_WIDTH - 1 : 0] inoutData;
+  reg [DATA_WIDTH - 1 : 0] inoutData;
 
-    tri [ADDR_WIDTH - 1 : 0] Address;
+  tri [ADDR_WIDTH - 1 : 0] Address;
 
-    tri Clock;
+  tri Clock;
 
-    tri Enable;
+  tri Enable;
 
-    tri Read;
+  tri Read;
 
-    tri Write;
+  tri Write;
 
-    tri Refresh;
+  tri Refresh;
 
-    tri Crystal50Mhz1;
+  tri Crystal50Mhz1;
 
-    tri Crystal50Mhz2;
+  tri Crystal50Mhz2;
 
-    
+  LinkPowerNoPLC FetchLEOS
+  (
+    Crystal50Mhz1,
+    Crystal50Mhz2
+  );
+  
     
     
 
