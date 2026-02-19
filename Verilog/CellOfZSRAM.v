300dc00ecadce9bf8dba034717477d5e08532ecf
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-02-20 04:57:24 +0900

    Add Crystal50Mhz1 and Crystal50Mhz2 inputs

--- Verilog/CellOfZSRAM.v
+++ Verilog/CellOfZSRAM.v
@@ -5,12 +5,17 @@ module CellOfZSRAM //Cell Of Zero Second RAM
   output outputData,
   input ReadEdge,
   input inputData,
-  input WriteEdge
+  input WriteEdge,
+  input Crystal50Mhz1,
+  input Crystal50Mhz2
+);
 );
   tri outputData;
   tri ReadEdge;
   tri inputData;
   tri WriteEdge;
+  tri Crystal50Mhz1;
+  tri Crystal50Mhz2;
 
   tri FromInputData;
 
