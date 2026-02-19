95b130727fc3bbb3d8ee7dcbef587ba278174d3a
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-02-20 04:31:07 +0900

    Add CellOfZSRAM module with basic I/O

---
+++ Verilog/CellOfZSRAM.v
@@ -0,0 +1,15 @@
+`include "DigitSupply.vh"
+
+module CellOfZSRAM
+(
+  output outputData,
+  input ReadEdge,
+  input inputData,
+  input WriteEdge
+);
+  tri outputData;
+  tri ReadEdge;
+  tri inputData;
+  tri WriteEdge;
+  
+endmodule 
