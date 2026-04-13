cd40f545979c33af5fbbed98110e8068e1ca94fc
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-04-13 11:38:26 +0900

    Fix syntax for pmos and nmos instantiation

--- Verilog/DDR_SRAM.v
+++ Verilog/DDR_SRAM.v
@@ -78,7 +78,7 @@ parameter DATA_WIDTH = 1
 
    
 
-    _pmos ChargeFor0(ChargedPullDigit[0], PullDigit[0],1`b0);
+ _pmos ChargeFor0(ChargedPullDigit[0], PullDigit[0],1'b0);
 
     _nmos ChargeFor1(ChargedPullDigit[1], PullDigit[1],1`b1);
 
