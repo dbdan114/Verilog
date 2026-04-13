fc95f870db4d5596163ea2680fa238276297a1ea
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-04-13 11:23:16 +0900

    Fix syntax for binary literals in SDR_QSRAM.v

--- Verilog/SDR_QSRAM.v
+++ Verilog/SDR_QSRAM.v
@@ -64,7 +64,7 @@ parameter DATA_WIDTH = 1
     
     PosEdge MakePosEdge(PosEdgeClock, SupplyDigit, Clock);
 
-    and_B_to_A(BufferedEnable, SupplyDigit, 1`b1, Enable);
+    and_B_to_A(BufferedEnable, SupplyDigit, 1'b1, Enable);
 
     and_B_to_A(AndedOfEdgeClock_Enable,SupplyDigit,BufferedEnable,PosEdgeClock);
 
