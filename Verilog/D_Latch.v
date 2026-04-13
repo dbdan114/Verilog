3c919d03aa14a8b7eb3c22ba89046ffbb4a27405
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-04-13 11:56:19 +0900

    Rename instances for ChangedReadEdge signals

--- Verilog/D_Latch.v
+++ Verilog/D_Latch.v
@@ -38,9 +38,9 @@ module D_Latch(
     _buf NorthToSouth(DataSouth,PullDigit,DataNorth);
     _buf SouthToNorth(DataNorth,PullDigit,DataSouth);
 
-    and_B_to_A(ChangedReadEdge0, SupplyDigit, ReadEdge, PullDigit[1]);
+    and_B_to_A MakeChangedReadEdge0(ChangedReadEdge0, SupplyDigit, ReadEdge, PullDigit[1]);
 
-    and_nB_to_A( ChangedReadEdge1, 2'b10, ChangedReadEdge0, PullDigit[0]);
+    and_nB_to_A MakeChangedReadEdge1(ChangedReadEdge1, 2'b10, ChangedReadEdge0, PullDigit[0]);
     _nmos NorthToEast(DataEast,DataNorth, ChangedReadEdge1);//SupplyDigit[1]);
     _nmos SouthToEast(DataEast,DataSouth, ChangedReadEdge1);//SupplyDigit[1]);
     _buf ToEast(outputData,2'b10,DataEast);
