6427a5762e03b43e851fb9800503e4fd726a5941
dbdan114 <113273313+dbdan114@users.noreply.github.com>
2026-04-17 15:56:32 +0900

    Add DifferentialQBit instances for signals

--- Verilog/PowerAntenna.v
+++ Verilog/PowerAntenna.v
@@ -4,5 +4,31 @@ module PowerAntenna
 (
   inout Antenna
 );
+  DifferentialQBit MakeSupplySignal0
+  (
+    SupplyDigit[1], 
+    SupplyDigit[1]
+  );
+  DifferentialQBit MakeSupplySignal1
+  (
+    SupplyDigit[1], 
+    SupplyDigit[0])
+  ;
+  
+  DifferentialQBit MakePullSignal1
+  (
+    SupplyDigit[0], 
+    PullDigit[1]
+  );
+  DifferentialQBit MakePullSignal0
+  (
+    SupplyDigit[0], 
+    PullDigit[0]
+  );
 
+  DifferentialQBit GetInOutSignal
+  (
+    Antenna, 
+    SupplyDigit[1]
+  );
 endmodule
